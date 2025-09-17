c9bfda2499f0aea806c2cbf711a641d025a87865
spcotoon <springcomicskr@gmail.com>
2025-09-17 23:00:39 +0900

    5번변경

--- src/main/java/egov/board/dao/BoardMapper.java
+++ src/main/java/egov/board/dao/BoardMapper.java
@@ -8,9 +8,9 @@ import org.egovframe.rte.psl.dataaccess.mapper.Mapper;
 @Mapper("BoardMapper")
 public interface BoardMapper {
 	void saveBoard(HashMap<String, Object> paramMap)throws Exception;
-
+//4번번경
 	HashMap<String, Object> showBoard(HashMap<String, Object> paramMap)throws Exception;
-
+//5번
 	ArrayList<HashMap<String, Object>> showBoardList(HashMap<String, Object> paramMap)throws Exception;
-
+//1번변경
 }
